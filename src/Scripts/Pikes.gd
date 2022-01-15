b9f96b5fcc0502c52fd5e080db2b8f60475e6490
LI Simon 236797 <SL236797@intra.cea.fr>
2022-01-15 12:57:21 +0100

    Knock back on damage

--- src/Scripts/Pikes.gd
+++ src/Scripts/Pikes.gd
@@ -2,6 +2,11 @@ extends Area2D
 
 var health = 5
 
+func knockback(player):
+	player.inputON = false
+	var RecoveryTimer = player.get_node("RecoveryTimer")
+	RecoveryTimer.start()
+	player.velocity = player.velocity.rotated(PI)
 
 func _process(delta):
 	if health<=0:
@@ -16,4 +21,7 @@ func _on_Pikes_area_entered(area):
 func _on_Pikes_body_entered(body):
 	var player = get_parent().get_node("Player")
 	if body==player:
+		# knock back
+		knockback(player)
+		# damage
 		player.health -=1
--- src/Scripts/PlayerManager.gd
+++ src/Scripts/PlayerManager.gd
@@ -5,9 +5,13 @@ export var health = 10
 export var speed = 200
 export var friction = 0.01
 export var acceleration = 0.1
+export var cadence = 0.2
+
 var velocity = Vector2()
 
 var Bullet = preload("res://Scenes/Bullet.tscn")
+
+onready var inputON = true
 
 func get_input():
 	var input = Vector2()
@@ -23,7 +27,7 @@ func get_input():
 	
 func shoot():
 	if $Cadence.is_stopped():
-		$Cadence.wait_time = 0.2
+		$Cadence.wait_time = cadence
 		$Cadence.start()
 		var b = Bullet.instance()
 		get_parent().add_child(b)
@@ -31,12 +35,13 @@ func shoot():
 	
 
 func _physics_process(delta):
-	look_at(get_global_mouse_position())
-	var direction = get_input()
-	if direction.length() > 0:
-		velocity = lerp(velocity, direction.normalized() * speed, acceleration)
-	else:
-		velocity = lerp(velocity, Vector2.ZERO, friction)
+	if inputON:
+		look_at(get_global_mouse_position())
+		var direction = get_input()
+		if direction.length() > 0:
+			velocity = lerp(velocity, direction.normalized() * speed, acceleration)
+		else:
+			velocity = lerp(velocity, Vector2.ZERO, friction)
 	velocity = move_and_slide(velocity)
 	
 	if Input.is_action_pressed("ui_mouseleft"):
@@ -44,3 +49,8 @@ func _physics_process(delta):
 
 func _on_Cadence_timeout():
 	$Cadence.stop()
+
+
+func _on_RecoveryTimer_timeout():
+	inputON=true
+	$RecoveryTimer.stop()

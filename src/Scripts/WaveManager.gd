0704b1256df248aa9e68d9a25ed218e6f0510b82
Georis Billo <vost@pop-os.localdomain>
2022-01-29 22:39:07 +0100

    wave machin

--- src/Scripts/WaveManager.gd
+++ src/Scripts/WaveManager.gd
@@ -25,8 +25,8 @@ onready var Oxy = get_global_transform_with_canvas().origin
 onready var myOffset = OS.window_size/5.5
 
 var currentWave = -1
-var timerTable = [30,30,30,40,60]
-#var timerTable = [1,1,1,1,1]
+#var timerTable = [30,30,30,40,60]
+var timerTable = [1,1,1,1,1]
 var populationDensity = [4,4,4,4,4]
 var waveColor = [Vector3(0.4,0.8,0.9),Vector3(0.4,0.8,0.6),Vector3(0.7,0.9,0.4),Vector3(1.0,0.7,0.2),Vector3(1.0,0.2,0.2)]
 var populationType = [	[50,0,0,0,0,0,0,0,0,0,50,0,0,0],
@@ -35,8 +35,8 @@ var populationType = [	[50,0,0,0,0,0,0,0,0,0,50,0,0,0],
 						[10,50,25,5,0,0,10,10,0,0,0,0,10,0],
 						[10,40,30,15,15,15,0,0,5,0,0,0,0,0]]
 var populateDeltaT = [5,5,4,4,4]
-var nbDMC = 10
-#var nbDMC = 1
+#var nbDMC = 10
+var nbDMC = 1
 var DMC_remain = 0
 
 var firstTimeWave0 = false
@@ -81,6 +81,8 @@ func _process(delta):
 			player.get_node("PlayerDialog").speak("TAKE THAT ! What now ?",3)
 			self.get_parent().get_node("Flash").flash()
 			yield(get_tree().create_timer(0.5), "timeout")
+			if !SoundManager.get_node("kaboom").is_playing():
+				SoundManager._play_song_from_name("kaboom")
 			var s = shockwave.instance()
 			self.add_child(s)
 			alertMessage("He has used his deadly weapon!\nDESTROY HIM NOW!",4.9)
@@ -108,7 +110,8 @@ func _process(delta):
 		if $Timer.is_stopped(): $Timer.start(5)
 	elif(currentWave==9):
 		alertMessage("Your body-shell has no more energy",5)
-		player.get_node("PlayerDialog").speak("It's over?",5)
+		#player.get_node("PlayerDialog").speak("It's over?",5)
+		player.get_node("AnimationTree").get("parameters/playback").travel("DEATH")
 		if $Timer.is_stopped(): $Timer.start(5)
 	elif(currentWave==10):
 		get_tree().change_scene("res://Scenes/Pre-credits.tscn")
